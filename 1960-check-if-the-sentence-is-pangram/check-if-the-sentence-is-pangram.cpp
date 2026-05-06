e5c22c1f47aa828b58b5a996e90807d7bf149492
ARJUN DUBEY <arjundubey.cg@gmail.com>
2026-05-06 16:54:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (6.95%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+
+        unordered_set<char>s(sentence.begin(),sentence.end());
+        return s.size()==26;
+
+
+
+
+
+
+        
+    }
+};
