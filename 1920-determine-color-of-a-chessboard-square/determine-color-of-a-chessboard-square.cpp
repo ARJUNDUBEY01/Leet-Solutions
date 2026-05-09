0764856620a8195ced45842cf6ba954877425df3
ARJUN DUBEY <arjundubey.cg@gmail.com>
2026-05-09 17:03:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (8.52%) - LeetSync

---
+++ 1920-determine-color-of-a-chessboard-square/determine-color-of-a-chessboard-square.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool squareIsWhite(string coordinates) {
+        int column=coordinates[0]-'a';
+        int row =coordinates[1]-'1';
+
+        return(column +row)%2==1;
+
+        
+
+
+
+        
+    }
+};
