ebb79607188366236454958deee8c38f29c197fe
ARJUN DUBEY <arjundubey.cg@gmail.com>
2026-05-24 12:57:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (62.89%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.cpp
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
-        
 
-        int count =0;
-        for ( char stone :stones  ){
-            for (char jewel:jewels){
-                if (stone==jewel){
+        int count=0;
+         for (char stone: stones){
+            for(char jewel:jewels){
+                if(stone==jewel){
                     count++;
                     break;
                 }
             }
-        }
-     return count ;
+         }
+         return count;
+
+      
     }
 };
