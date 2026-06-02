8cce2eccb47f1e037a72fdad251f1ef96dfd97f4
ARJUN DUBEY <arjundubey.cg@gmail.com>
2026-06-02 11:25:32 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (78.75%) - LeetSync

--- 1610-xor-operation-in-an-array/xor-operation-in-an-array.cpp
+++ 1610-xor-operation-in-an-array/xor-operation-in-an-array.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     int xorOperation(int n, int start) {
 
-        int result =0;
-        for (int i=0;i<n;i++){
-            result^=(start+2*i);
+      
+      int result =0;
+      for (int i=0;i<n;i++){
+        result^=(start+2*i);
 
-        }
-        return result;
+      }
+      return result;
     }
 };
