085915e945f51e5c5c91df23d5e0a9726b4d3b2e
ARJUN DUBEY <arjundubey.cg@gmail.com>
2026-06-06 13:52:28 +0530

    Time: 1 ms (91.71%) | Memory: 15.1 MB (26.63%) - LeetSync

---
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+        vector<pair<char, int>> st;
+
+        for (char ch : s) {
+            if (!st.empty() && st.back().first == ch) {
+                st.back().second++;
+
+                if (st.back().second == k)
+                    st.pop_back();
+            }
+            else {
+                st.push_back({ch, 1});
+            }
+        }
+
+        string ans = "";
+
+        for (auto p : st) {
+            ans += string(p.second, p.first);
+        }
+
+        return ans;
+        
+    }
+};
