f7f63a97d0c3735ef2bb8c48851b6666638891d3
ARJUN DUBEY <arjundubey.cg@gmail.com>
2026-06-28 22:57:57 +0530

    Time: 0 ms (100.00%) | Memory: 102 MB (84.43%) - LeetSync

---
+++ 463-island-perimeter/island-perimeter.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int land = 0, shared = 0;
+
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == 1) {
+                    land++;
+
+                    // Check down
+                    if (i + 1 < grid.size() && grid[i + 1][j] == 1)
+                        shared++;
+
+                    // Check right
+                    if (j + 1 < grid[0].size() && grid[i][j + 1] == 1)
+                        shared++;
+                }
+            }
+        }
+
+        return land * 4 - shared * 2;
+    }
+};
